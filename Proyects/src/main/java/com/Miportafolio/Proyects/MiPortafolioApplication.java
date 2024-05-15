04d7cd7aa65b79af950a27e45d4b189b050d9fd0
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2024-05-15 10:39:22 -0500

    Project 2.0

---
+++ Proyects/src/main/java/com/Miportafolio/Proyects/MiPortafolioApplication.java
@@ -0,0 +1,13 @@
+package com.Miportafolio.Proyects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPortafolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPortafolioApplication.class, args);
+	}
+
+}
---
+++ Proyects/src/test/java/com/Miportafolio/Proyects/MiPortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Miportafolio.Proyects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
